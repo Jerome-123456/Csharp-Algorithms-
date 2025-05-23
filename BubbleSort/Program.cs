99e7ef40a552699164dc4270ce4f28c421bdcd5b
Jerome-123456 <DAmbrose25@synergymat.uk>
2025-05-23 09:25:05 +0100

    Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] List = { 5, 3, 8, 4, 2 };
+for (int i = 1; i < List.Length; i++)
+{
+    for (int j = 0; j < List.Length - i; j++)
+    {
+        if (List[j] > List[j + 1])
+        {
+            int temp = List[j];
+            List[j] = List[j + 1];
+            List[j + 1] = temp;
+        }
+    }
+}
+foreach (int i in List)
+{
+    Console.Write(i + " ");
+}
