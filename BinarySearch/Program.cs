907f2e3b6208349e84a56f0b7e496020549c9468
Jerome-123456 <DAmbrose25@synergymat.uk>
2025-06-02 12:33:11 +0100

    searchs added

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] List = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int searchValue = 4;
+bool found = false;
+int middle = List.Length / 2;
+if (List.Length == 0)
+{
+    Console.WriteLine("List is empty.");
+}
+else
+{
+    while (middle >= 0 && middle < List.Length)
+    {
+        if (List[middle] == searchValue)
+        {
+            found = true;
+            Console.WriteLine($"Value {searchValue} found at index {middle}.");
+            break;
+        }
+        else if (List[middle] < searchValue)
+        {
+            middle++;
+        }
+        else
+        {
+            middle--;
+        }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] List = { 5, 3, 8, 4, 2 };
+int searchValue = 4;
+bool found = false;
+for (int i = 0; i < List.Length; i++)
+{
+    if (List[i] == searchValue)
+    {
+        found = true;
+        Console.WriteLine($"Value {searchValue} found at index {i}.");
+        break;
+    }
+}
